3d3be80461ceaef0b445a166c3d466decf2f0a00
Glitch (hello-webpage) <none>
2020-04-14 21:53:46 +0000

    🚟🐽 Checkpoint ./server.js:8045597/60

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ if (process.env.STATUS === 'online') {
  
 // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
 // see https://expressjs.com/en/guide/behind-proxies.html
-// app.set('trust proxy', 1);
+app.set('trust proxy', 1);
  
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 15 * 60, // 15 minutes
   max: 100 // limit each IP to 100 requests per windowMs
 });
  
