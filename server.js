0871c102b6eab6415e20b388961a72c2cebb40e8
Glitch (hello-webpage) <none>
2020-04-14 16:05:33 +0000

    🎂👯 Checkpoint ./server.js:8045597/2656

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express');
 const app = express();
+if (process.env.STATUS === 'online') {
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
+
+// Static Files
 app.use(express.static(process.env.MAINDIR));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// Basic Routing & Page Rules
+  
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/src/index.*');
 });
 
+  // API 
 app.get(process.env.API, (req, res) => res.send('200 OK'))
-// listen for requests :)
+
+  // Start the Server
+  
+  
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+}
+
+// This is the offliine status
+if (process.env.STATUS === 'offline') {
+  app.get(process.env.MAINDIR, (req, res) => res.send('This server is Offline'))
+}
