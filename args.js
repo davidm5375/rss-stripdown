5322e21b55e25d0c3d0d9e2e14740b187e482734
Glitch (hello-webpage) <none>
2020-04-14 03:39:10 +0000

    ⛹💧 Checkpoint ./server.js:8045597/99 ./src/index.html:8045597/7341 ./package.json:8045597/134

--- args.js
+++
@@ -1,6 +0,0 @@
-
-const two = process.argv.slice(2)
-const three = process.argv.slice(3)
-
-console.log(two)
-console.log(three)
--- server.js
+++ server.js
@@ -9,13 +9,14 @@ const app = express();
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static(process.env.DIR));
+app.use(express.static(process.env.MAINDIR));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/src/index.*');
 });
 
+app.get(process.env.API, (req, res) => res.send('200 OK'))
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
