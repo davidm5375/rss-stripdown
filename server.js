72d3dcc74591ad97d03ca327e1c1422949b6fa64
Glitch (hello-webpage) <none>
2020-04-14 00:33:22 +0000

    🚝🎠 Checkpoint ./index.js:8045597/1451 ./package.json:8045597/1886 ./blog-single.html:8045597/33504 ./src/index.html:8045597/12546

--- server.js
+++ server.js
@@ -13,11 +13,9 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/src/index.*');
+  response.sendFile(__dirname + '/src/index.html');
 });
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/src/status.*');
-});
+
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
