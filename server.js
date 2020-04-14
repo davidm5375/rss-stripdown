7fed3aa2364c135881b4978032c77dacff4f68b4
Glitch (hello-webpage) <none>
2020-04-14 21:41:39 +0000

    🍟🎟 Checkpoint ./server.js:8045597/408

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const app = express();
 // This is the "online" status
 if (process.env.STATUS === 'online') {
 
+  const rateLimit = require("express-rate-limit");
+ 
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+// app.set('trust proxy', 1);
+ 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+ 
+//  apply to all requests
+app.use(limiter);
 
 
 // Static Files
