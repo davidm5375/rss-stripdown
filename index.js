019b4125690aeb4a8d836f233f05417da4fc5578
Glitch (rss-npm) <none>
2020-04-15 19:03:21 +0000

    🏊🏼🚿 Checkpoint ./server.js:8045597/320 ./index.js:8045597/34 ./package.json:8045597/20

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 var server = require("./server.js");
-server.rateLimiter(100, 60000);
 server.startServer(3000, "./src");
--- server.js
+++ server.js
@@ -1,16 +1,5 @@
 const express = require("express");
 const app = express();
-const rateLimit = require("express-rate-limit");
-function rateLimiter(request, ms) {
-  app.set("trust proxy", 1);
-
-  const limiter = rateLimit({
-    windowMs: ms,
-    max: request
-  });
-  //  apply to all requests
-  app.use(limiter);
-}
 function startServer(port, dir) {
   app.use(express.static(dir));
   const listener = app.listen(port, function() {
@@ -21,5 +10,4 @@ function addRoute(route, path){
   app.get(route, (request, response) => {
   response.sendFile(__dirname + path);
 });}
-module.exports = {rateLimiter, startServer, addRoute} // files will be able to access everything you put in there.
-// thanks
+module.exports = {startServer, addRoute}
