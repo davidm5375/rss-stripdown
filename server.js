d52912577ce179ef38494c013400ad92507a51b6
Glitch (reallysimpleserver) <none>
2020-04-15 16:00:39 +0000

    🏣📹 Checkpoint ./server.js:8045597/1032

--- server.js
+++ server.js
@@ -34,8 +34,18 @@ if (process.env.STATUS === "offline") {
     response.sendFile(__dirname + "/api/status/offline.html");
   });
   
+// Plugins - https://github.com/really-simple-server/plugins
+      // webDAV server
+  const webdav = require("webdav-server").v2;
+  const server = new webdav.WebDAVServer();
+  app.use(webdav.extensions.express("/", server));
+  // User manager
+  const userManager = new webdav.SimpleUserManager();
+  const user = userManager.addUser(process.envFTPUSER, process.envFTPPASS, false);
+  // Privileges
+  const privilegeManager = new webdav.SimplePathPrivilegeManager();
+  privilegeManager.setRights(user, "/", ["all"]); 
 
-  
   
   // Booting up the server
   const listener = app.listen(process.env.PORT, function() {
