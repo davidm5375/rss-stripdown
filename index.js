9da3772b4c0c8b096acd4f2bf308b6ee06cc29c4
Glitch (reallysimpleserver) <none>
2020-04-15 17:41:25 +0000

    💠🖇 Checkpoint ./index.js:8045597/42

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const server = require("server.js");
-var limit = express.rateLimiter(100, 60000);
-var server = express.startServer(3000, src);
+const rss = require(".server.js");
+var limit = rss.rateLimiter(100, 60000);
+var server = rss.startServer(3000, src);
