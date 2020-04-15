d32d507c1ea6e47672eca9586ab3bac1b30324d7
Glitch (rss-npm) <none>
2020-04-15 18:51:21 +0000

    🌁📮 Checkpoint ./index.js:2731514/668:8045597/3118 ./server.js:2731514/771:8045597/948

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 const rateLimit = require("express-rate-limit");
-
-module.exports.rateLimiter = (request, ms) => {
+function rateLimiter(request, ms) {
   app.set("trust proxy", 1);
 
   const limiter = rateLimit({
@@ -12,35 +11,15 @@ module.exports.rateLimiter = (request, ms) => {
   //  apply to all requests
   app.use(limiter);
 }
-// function rateLimiter(request, ms) {
-//   app.set("trust proxy", 1);
-
-//   const limiter = rateLimit({
-//     windowMs: ms,
-//     max: request
-//   });
-//   //  apply to all requests
-//   app.use(limiter);
-// }
-
-module.exports.startServer = (port, dir) => {
+function startServer(port, dir) {
   app.use(express.static(dir));
   const listener = app.listen(port, function() {
     console.log("Your app is listening on port " + listener.address().port);
   });
 }
-// function startServer(port, dir) {
-//   app.use(express.static(dir));
-//   const listener = app.listen(port, function() {
-//     console.log("Your app is listening on port " + listener.address().port);
-//   });
-// }
-
-module.exports.addRoute = (route, path) => {
+function addRoute(route, path){
   app.get(route, (request, response) => {
   response.sendFile(__dirname + path);
 });}
-// function addRoute(route, path){
-//   app.get(route, (request, response) => {
-//   response.sendFile(__dirname + path);
-// });}
+module.exports = {rateLimiter, startServer, addRoute} // files will be able to access everything you put in there.
+// thanks
