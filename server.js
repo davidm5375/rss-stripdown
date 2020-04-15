5e32bae2a06e1882c843dcd20d749406d83a0b8f
Glitch (rss-npm) <none>
2020-04-15 17:53:21 +0000

    🌾😺 Checkpoint ./server.js:8045597/204

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const rateLimit = require("express-rate-limit");
 
-module.exports = {
-rateLimiter: function(request, ms) {
+
+function rateLimiter(request, ms) {
   app.set("trust proxy", 1);
 
   const limiter = rateLimit({
@@ -13,14 +13,13 @@ rateLimiter: function(request, ms) {
   //  apply to all requests
   app.use(limiter);
 }
-startServer: function(port, dir) {
+function startServer(port, dir) {
   app.use(express.static(dir));
   app.get("/", function(request, response) {
     response.sendFile(__dirname + "/" + dir + "/index.*");
   });
-  app.get(process.env.STATUSURL, (req, res) => res.send("200 OK"));
+  app.get('/serverstatus102', (req, res) => res.send("200 OK"));
   const listener = app.listen(port, function() {
     console.log("Your app is listening on port " + listener.address().port);
   });
-}
 }
